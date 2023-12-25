c3e5ed4082ca02e87c87baeece98c5d3000c2825
Shivam Gupta <shivamgupta@Shivams-MacBook-Air.local>
2023-12-26 00:31:10 +0530

    removed data

--- src/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const expres = require("express");
-const router = expres.Router();
-const whatsAppController = require("../controllers/whatsappControllers");
-
-router
-.get("/", whatsAppController.VerifyToken)
-.post("/", whatsAppController.ReceivedMessage)
-
-module.exports = router;
