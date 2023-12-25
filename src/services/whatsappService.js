ca22e449ca549df967fcaac10bf688c26c43ac55
Shivam Gupta <shivamgupta@Shivams-MacBook-Air.local>
2023-12-26 00:23:29 +0530

    added sample project

---
+++ src/services/whatsappService.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const myConsole = new console.Console(fs.createWriteStream("./logs.txt"));
+const https = require("https");
+function SendMessageWhatsApp(data){
+    
+    const options = {
+        host: "graph.facebook.com",
+        path: "/v13.0/{YOUR_ID}/messages",
+        method: "POST",
+        body: data,
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer {YOUR_TOKEN}"
+        }
+    };
+    const req = https.request(options, res => {
+        res.on("data", d=> {
+            process.stdout.write(d);
+        });
+    });
+
+    req.on("error", error => {
+        console.error(error);
+    });
+
+    req.write(data);
+    req.end();
+}
+
+module.exports = {
+    SendMessageWhatsApp
+};
